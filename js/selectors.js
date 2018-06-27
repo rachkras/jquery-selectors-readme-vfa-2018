2d587b6d23dad13febe9fe0bf04575289213c846
Rachel Krasnick <rachkras@umich.edu>
2018-06-27 21:08:28 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
-// declare your functions here...
+function paragraphSelectors(){
+  return $("p");
+}
+
+function lastImageSelector(){
+  return $("img:last");
+}
+
+function ninjaBabySelector(){
+  return $("#baby-ninja");
+}
+
+function divSelector(){
+  return $(".pic:nth-child(1)")+$(".pic:nth-child(2)");
+} 
+
+function firstListItem(){
+  return $("#pic-list li:first-child");
+}
